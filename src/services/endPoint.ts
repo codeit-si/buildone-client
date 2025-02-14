81f7c95c5c5c51216925ea14208fff990a5bc4aa
ChaeYubin <cyoobin@naver.com>
2025-02-14 14:50:52 +0900

    [DEV-77] chore: axios instance 생성 및 zustand 설치

---
+++ src/services/endPoint.ts
@@ -0,0 +1,12 @@
+export const apiEndPoint = {
+  auth: {
+    login: "/auth/login",
+    signUp: "/auth/signup",
+  },
+  goal: {
+    getGoals: "/goals",
+  },
+  todo: {
+    getTodos: "/todos",
+  },
+};
