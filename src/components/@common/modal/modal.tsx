00f12615dbda30d5cf785a1921d6faaaf9ef8ea8
hvrain <hvrain3222@naver.com>
2025-02-14 10:21:51 +0900

    [DEV-48] fix: 오타와 중복선언 제거

--- src/components/@common/modal/modal.tsx
+++ src/components/@common/modal/modal.tsx
@@ -30,10 +30,6 @@ type ModalElementProps<T extends ElementType> = ComponentProps<T> & {
   asChild?: boolean;
 };
 
-interface ModalCommonProps {
-  className?: string;
-}
-
 const modalContext = createContext<ModalContextProps>({
   open: false,
   setOpen: () => {},
@@ -75,7 +71,7 @@ function ModalRoot({
 
     document.addEventListener("keydown", handleEscapeKey);
     return () => document.removeEventListener("keydown", handleEscapeKey);
-  }, []);
+  }, [setOpen]);
 
   const providerValue = useMemo(
     () => ({
@@ -130,12 +126,12 @@ function ModalPortal({
 }
 
 interface ModalOverlayProps {
-  closeOnOverlyClick?: boolean;
+  closeOnOverlayClick?: boolean;
 }
 
 function ModalOverlay({
   className,
-  closeOnOverlyClick = true,
+  closeOnOverlayClick = true,
 }: ModalCommonProps & ModalOverlayProps) {
   const { setOpen } = useModal();
 
@@ -145,7 +141,7 @@ function ModalOverlay({
       aria-hidden
       className={cn("fixed inset-0 bg-black opacity-50", className)}
       onClick={() => {
-        if (closeOnOverlyClick) setOpen(false);
+        if (closeOnOverlayClick) setOpen(false);
       }}
     />
   );
