7106702829cf661e62613fd730ec52212b11bda2
ChaeYubin <cyoobin@naver.com>
2025-02-17 14:38:39 +0900

    [DEV-37] refactor: endpoint prefix 변수화

--- src/services/endpoint.ts
+++ src/services/endpoint.ts
@@ -1,17 +1,20 @@
+const PREFIX = "/api/v1";
+
 export const ENDPOINT = {
   AUTH: {
-    LOGIN: "/api/login",
-    SIGNUP: "/api/signup",
+    LOGIN: `${PREFIX}/login`,
+    SIGNUP: `${PREFIX}/signup`,
   },
+  // 아래 endpoint들은 예시입니다!
   GOAL: {
     GET_ALL: "/api/goals",
-    GET_BY_ID: (id: number) => `/api/goals/${id}`,
+    GET_BY_ID: (id: number) => `${PREFIX}/goals/${id}`,
     CREATE: "/api/goals",
-    UPDATE: (id: number) => `/api/goals/${id}`,
-    DELETE: (id: number) => `/api/goals/${id}`,
+    UPDATE: (id: number) => `${PREFIX}/goals/${id}`,
+    DELETE: (id: number) => `${PREFIX}/goals/${id}`,
   },
   TODO: {
     GET_ALL: "/api/todos",
-    GET_BY_ID: (id: number) => `/api/todos/${id}`,
+    GET_BY_ID: (id: number) => `${PREFIX}/todos/${id}`,
   },
 } as const;
