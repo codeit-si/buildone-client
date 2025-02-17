d28763682f143e32d7649d61ad88b62dafbac940
ChaeYubin <cyoobin@naver.com>
2025-02-17 21:39:00 +0900

    [DEV-37] style: 문장 끝 온점 일괄적으로 추가

--- src/containers/login/login-form.tsx
+++ src/containers/login/login-form.tsx
@@ -19,9 +19,9 @@ import { useUserActions } from "@/store/userStore";
 const loginSchema = z.object({
   email: z
     .string()
-    .nonempty("이메일을 입력해주세요")
-    .email("이메일 형식을 입력해주세요"),
-  password: z.string().nonempty("비밀번호를 입력해주세요"),
+    .nonempty("이메일을 입력해주세요.")
+    .email("이메일 형식을 입력해주세요."),
+  password: z.string().nonempty("비밀번호를 입력해주세요."),
 });
 
 type LoginSchema = z.infer<typeof loginSchema>;
