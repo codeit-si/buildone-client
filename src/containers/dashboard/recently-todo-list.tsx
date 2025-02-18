00430e3ebfae4ceb658a31a1e9748e14429c017e
hvrain <hvrain3222@naver.com>
2025-02-18 19:15:17 +0900

    [DEV-56] feat: scrollbar 추가 & scroll 요소 blur 효과 추가

--- src/containers/dashboard/recently-todo-list.tsx
+++ src/containers/dashboard/recently-todo-list.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
+
 import { useSuspenseQuery } from "@tanstack/react-query";
 
+import { cn } from "@/lib/cn";
 import { getDashboardOptions } from "@/services/dashboard";
 
 import Goal from "./goal";
@@ -10,21 +13,37 @@ import TodoIcons from "./todo-icons";
 
 export default function RecentlyTodoList() {
   const { data } = useSuspenseQuery(getDashboardOptions());
+  const listRef = useRef<HTMLUListElement | null>(null);
+  const [isOverflowing, setIsOverflowing] = useState(false);
   const { todos } = data;
 
+  useEffect(() => {
+    if (listRef.current) {
+      setIsOverflowing(listRef.current.scrollHeight > 170);
+    }
+  }, [todos]);
+
   return (
-    <ul className="flex flex-col gap-8">
-      {todos.map((todo) => (
-        <li key={todo.id} className="text-sm">
-          <div className="flex items-center justify-between">
-            <RecentlyTodoCheckbox todo={todo} />
-            <TodoIcons todo={todo} />
-          </div>
-          {todo.goalInformation && (
-            <Goal goal={todo.goalInformation} isDone={todo.isDone} />
-          )}
-        </li>
-      ))}
-    </ul>
+    <div className="scrollbar relative h-full overflow-y-auto">
+      <ul ref={listRef} className="flex flex-col gap-8 pr-8">
+        {todos.map((todo) => (
+          <li key={todo.id} className="text-sm">
+            <div className="z-50 flex items-center justify-between">
+              <RecentlyTodoCheckbox todo={todo} />
+              <TodoIcons todo={todo} />
+            </div>
+            {todo.goalInformation && (
+              <Goal goal={todo.goalInformation} isDone={todo.isDone} />
+            )}
+          </li>
+        ))}
+      </ul>
+      <div
+        className={cn(
+          "pointer-events-none bottom-0 -mt-10 h-30 w-full bg-gradient-to-t from-white from-30% to-white/0",
+          isOverflowing ? "sticky" : "hidden",
+        )}
+      />
+    </div>
   );
 }
