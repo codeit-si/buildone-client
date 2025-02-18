3705c6468896243dfd344d0f28f781b259528d8b
hvrain <hvrain3222@naver.com>
2025-02-18 19:15:17 +0900

    [DEV-56] feat: scrollbar 추가 & scroll 요소 blur 효과 추가

--- src/containers/dashboard/recently-todo-container.tsx
+++ src/containers/dashboard/recently-todo-container.tsx
@@ -7,19 +7,21 @@ import RecentlyTodoList from "./recently-todo-list";
 
 export default function RecentlyTodoContainer() {
   return (
-    <section className="flex h-258 w-full min-w-343 flex-col gap-16 rounded-2xl bg-white p-16">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-8 text-base font-semibold">
-          <RecentlyTodoIcon />
-          <h2>최근 등록한 할 일</h2>
+    <section className="rounded-2xl bg-white">
+      <div className="relative flex h-258 w-full min-w-343 flex-col gap-16 bg-white p-16">
+        <div className="flex items-center justify-between pr-4">
+          <div className="flex items-center gap-8 text-base font-semibold">
+            <RecentlyTodoIcon />
+            <h2>최근 등록한 할 일</h2>
+          </div>
+          <div className="flex items-center justify-center gap-8 text-sm font-medium text-slate-600">
+            모두 보기 <RightArrow />
+          </div>
         </div>
-        <div className="flex items-center justify-center gap-8 text-sm font-medium text-slate-600">
-          모두 보기 <RightArrow />
-        </div>
+        <Suspense fallback={<div>RecentlyTodoList 로딩중</div>}>
+          <RecentlyTodoList />
+        </Suspense>
       </div>
-      <Suspense fallback={<div>RecentlyTodoList 로딩중</div>}>
-        <RecentlyTodoList />
-      </Suspense>
     </section>
   );
 }
