6f736bd2bcebe3ca4cbdb4b920bc327cf0b184e4
hvrain <71882441+hvrain@users.noreply.github.com>
2025-02-18 14:05:17 +0900

    [DEV-52] Sheet & Popup 구현 (#91)
    * [DEV-48] feat: add basic function modal components
    
    * [DEV-48] feat: create modal components with headless
    
    * [DEV-48] feat: add modal additional component & add splitChildrenByComponent util
    
    * [DEV-48] feat: add some paths in tsconfig
    
    * [DEV-48] feat: add slot component for asChild property
    
    * [DEV-48] feat: apply slot in modal & make modal usage
    
    * [DEV-48] fix: modal close component에 onClose 추가 & 리뷰 반영
    
    * [DEV-48] feat: add modal additional component & add splitChildrenByComponent util
    
    * [DEV-48] fix: package.json 의존성 추가
    
    * [DEV-48] feat: add basic function modal components
    
    * [DEV-48] feat: create modal components with headless
    
    * [DEV-48] feat: add modal additional component & add splitChildrenByComponent util
    
    * [DEV-48] feat: apply slot in modal & make modal usage
    
    * [DEV-48] fix: modal close component에 onClose 추가 & 리뷰 반영
    
    * [DEV-48] feat: add modal additional component & add splitChildrenByComponent util
    
    * [DEV-48] feat: apply slot in modal & make modal usage
    
    * [DEV-48] fix: modal close component에 onClose 추가 & 리뷰 반영
    
    * [DEV-48] fix: modal 접근성 수정 (code rabbit 추천)
    
    * [DEV-48] fix: 오타와 중복선언 제거
    
    * [DEV-74] feat: add two case for modal (modal, popup)
    
    * [DEV-74] feat: 모달 애니메이션 적용
    
    * [DEV-74] fix: code rabbit 피드백 반영
    
    * [DEV-74] fix: modal overlay useEffect 수정
    
    * [DEV-52] fix: 74 rebase 후 lint 에러 수정
    
    * [DEV-52] fix: 내가 작성한 axios & tanstack 예시 코드 삭제
    
    * [DEV-52] feat: usePortalOpen 훅 구현
    
    * [DEV-52] feat: add usePortalClosesByEscapeKey 훅 추가
    
    * [DEV-52] feat: custom portal hooks 적용, portal 컴포넌트들 스타일 수정
    
    * [DEV-52] feat: sheet, popup 추가 & animation 수정
    
    * [DEV-52] fix: portal 컴포넌트들의 사용처를 분리하기 위해 각 컴포넌트 수정
    
    * [DEV-52] fix: popup 컴포넌트 스타일 수정
    
    * [DEV-52] fix: 코드 래빗 코멘트 적용 & 애니메이션 시간 조정
    
    * [DEV-52] fix: modal & popup button 컴포넌트 적용
    
    * [DEV-52] feat: popup 컴포넌트 버튼 스타일 추가

---
+++ src/components/@common/portal/popup.tsx
@@ -0,0 +1,229 @@
+"use client";
+
+import {
+  ComponentProps,
+  createContext,
+  ElementType,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { createPortal } from "react-dom";
+
+import { AnimatePresence } from "motion/react";
+import * as motion from "motion/react-client";
+
+import Button, { ButtonProps } from "@/components/button";
+import usePortalClosesByEscapeKey from "@/hooks/portal/use-portal-closes-by-escape-key";
+import usePortalOpen from "@/hooks/portal/use-portal-open";
+import { cn } from "@/lib/cn";
+import splitChildrenByComponents from "@/utils/react-child-utils/split-children-by-component";
+
+import Slot from "../slot/slot";
+
+type PopupContextProps = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+interface PopupCommonProps {
+  className?: string;
+}
+
+type PopupElementProps<T extends ElementType> = ComponentProps<T> & {
+  asChild?: boolean;
+};
+
+const popupContext = createContext<PopupContextProps>({
+  open: false,
+  setOpen: () => {},
+});
+
+function usePopup() {
+  const context = useContext(popupContext);
+  if (!context) throw new Error("usePopup must be used within a PopupProvider");
+  return context;
+}
+
+interface PopupRootProps {
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+}
+
+function PopupRoot({
+  children,
+  open: openProp = false,
+  onOpenChange,
+}: PropsWithChildren<PopupRootProps>) {
+  const { open, setOpen } = usePortalOpen(openProp, onOpenChange);
+
+  usePortalClosesByEscapeKey(open, setOpen);
+
+  const providerValue = useMemo(
+    () => ({
+      open,
+      setOpen,
+    }),
+    [open, setOpen],
+  );
+
+  return (
+    <popupContext.Provider value={providerValue}>
+      {children}
+    </popupContext.Provider>
+  );
+}
+
+function PopupTrigger({
+  children,
+  className,
+  asChild = false,
+}: PopupElementProps<"button">) {
+  const { setOpen } = usePopup();
+  const Comp = asChild ? Slot : "button";
+
+  return (
+    <Comp
+      className={cn("", className)}
+      type="button"
+      onClick={() => setOpen(true)}
+    >
+      {children}
+    </Comp>
+  );
+}
+
+function PopupPortal({ children }: PropsWithChildren) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const ChildrenWithCloseButton = children;
+
+  if (!isClient) return null;
+
+  return createPortal(ChildrenWithCloseButton, document.body);
+}
+
+interface PopupOverlayProps {
+  closeOnOverlayClick?: boolean;
+}
+
+function PopupOverlay({
+  className,
+  closeOnOverlayClick = true,
+}: PopupCommonProps & PopupOverlayProps) {
+  const { open, setOpen } = usePopup();
+
+  return (
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          role="presentation"
+          aria-hidden
+          className={cn("fixed inset-0 bg-gray-500", className)}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 0.5, transition: { duration: 0.1 } }}
+          exit={{ opacity: 0, transition: { duration: 0.1 } }}
+          onClick={() => {
+            if (closeOnOverlayClick) setOpen(false);
+          }}
+        />
+      )}
+    </AnimatePresence>
+  );
+}
+
+function PopupContent({ children, className }: ComponentProps<"div">) {
+  const { open } = usePopup();
+  const [[footer], nonContentChild] = splitChildrenByComponents(
+    [PopupFooter],
+    children,
+  );
+
+  return (
+    <PopupPortal>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            role="dialog"
+            aria-modal="true"
+            className={cn(
+              "fixed left-[50%] top-[50%] z-50 flex w-full max-w-300 translate-x-[-50%] translate-y-[-50%] flex-col gap-24 rounded-lg border bg-white px-16 py-24 pb-26 pt-24 shadow-xl md:max-w-450 md:px-24",
+              className,
+            )}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 0.1 } }}
+            exit={{ opacity: 0, transition: { duration: 0.1 } }}
+          >
+            <div />
+            {nonContentChild}
+            {footer}
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <PopupOverlay />
+    </PopupPortal>
+  );
+}
+
+function PopupFooter({
+  children,
+  className,
+  asChild = false,
+}: PopupElementProps<"div">) {
+  const Comp = asChild ? Slot : "div";
+
+  return (
+    <Comp className={cn("mt-16 flex w-full justify-center gap-8", className)}>
+      {children}
+    </Comp>
+  );
+}
+
+interface PopupCloseProps extends ButtonProps {
+  onClick?: (e: React.MouseEvent<Element, MouseEvent>) => void;
+}
+
+function PopupClose({
+  children,
+  className,
+  onClick = () => {},
+  shape = "square",
+  size = "sm",
+  ...props
+}: PopupCloseProps) {
+  const { setOpen } = usePopup();
+
+  const handleClick = (e: React.MouseEvent<Element, MouseEvent>) => {
+    onClick(e);
+    setOpen(false);
+  };
+
+  return (
+    <Button
+      className={cn("h-48 w-full max-w-120", className)}
+      shape={shape}
+      size={size}
+      onClick={handleClick}
+      aria-label="팝업 닫기"
+      {...props}
+    >
+      {children}
+    </Button>
+  );
+}
+
+const Popup = {
+  Root: PopupRoot,
+  Trigger: PopupTrigger,
+  Content: PopupContent,
+  Footer: PopupFooter,
+  Close: PopupClose,
+};
+
+export default Popup;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,7 +31,6 @@ const config: Config = {
       ],
     },
     screens: {
-      modal: "520px",
       md: "744px",
       lg: "1200px",
     },
