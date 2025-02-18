56beabcb9cc892a433e77263bd25a0e24a2f7121
typeyu <mytypeyu@gmail.com>
2025-02-18 20:53:48 +0900

    [DEV-55] refactor: 코드래빗 피드백 반영

--- src/containers/note/toolbar.tsx
+++ src/containers/note/toolbar.tsx
@@ -10,6 +10,7 @@ import H4Icon from "@/assets/editor/h4.svg";
 import H5Icon from "@/assets/editor/h5.svg";
 import ItalicIcon from "@/assets/editor/italic.svg";
 import UnderlineIcon from "@/assets/editor/underline.svg";
+import { cn } from "@/lib/cn";
 import "@/styles/note.css";
 
 interface ToolbarProps {
@@ -20,15 +21,20 @@ interface ToolbarButtonProps {
   onClick: () => void;
   IconComponent: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   ariaLabel: string;
+  isActive: boolean;
 }
 
 function ToolbarButton({
   onClick,
   IconComponent,
   ariaLabel,
+  isActive,
 }: ToolbarButtonProps) {
   return (
-    <button onClick={onClick} className="rounded p-2">
+    <button
+      onClick={onClick}
+      className={cn("rounded p-2", { "bg-slate-400": isActive })}
+    >
       <IconComponent className="h-25 w-25" aria-label={ariaLabel} />
     </button>
   );
@@ -42,41 +48,49 @@ function Toolbar({ editor }: ToolbarProps) {
       onClick: () => editor.chain().focus().toggleBold().run(),
       IconComponent: BoldIcon,
       ariaLabel: "Bold",
+      isActive: editor.isActive("bold"),
     },
     {
       onClick: () => editor.chain().focus().toggleItalic().run(),
       IconComponent: ItalicIcon,
       ariaLabel: "Italic",
+      isActive: editor.isActive("italic"),
     },
     {
       onClick: () => editor.chain().focus().toggleUnderline().run(),
       IconComponent: UnderlineIcon,
       ariaLabel: "Underline",
+      isActive: editor.isActive("underline"),
     },
     {
       onClick: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
       IconComponent: H1Icon,
       ariaLabel: "H1",
+      isActive: editor.isActive("heading", { level: 1 }),
     },
     {
       onClick: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
       IconComponent: H2Icon,
       ariaLabel: "H2",
+      isActive: editor.isActive("heading", { level: 2 }),
     },
     {
       onClick: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
       IconComponent: H3Icon,
       ariaLabel: "H3",
+      isActive: editor.isActive("heading", { level: 3 }),
     },
     {
       onClick: () => editor.chain().focus().toggleHeading({ level: 4 }).run(),
       IconComponent: H4Icon,
       ariaLabel: "H4",
+      isActive: editor.isActive("heading", { level: 4 }),
     },
     {
       onClick: () => editor.chain().focus().toggleHeading({ level: 5 }).run(),
       IconComponent: H5Icon,
       ariaLabel: "H5",
+      isActive: editor.isActive("heading", { level: 5 }),
     },
   ];
 
@@ -88,6 +102,7 @@ function Toolbar({ editor }: ToolbarProps) {
           onClick={btn.onClick}
           IconComponent={btn.IconComponent}
           ariaLabel={btn.ariaLabel}
+          isActive={btn.isActive}
         />
       ))}
     </div>
