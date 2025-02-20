c25dbfa806390e14e55d8ab8b2c49faccd59e6c1
typeYu <105202875+typeYu@users.noreply.github.com>
2025-02-20 10:17:36 +0900

    [Dev 57] feat: 링크 첨부 (#114)
    * [DEV-55] feat: 노트 작성 페이지 추가
    
    * [DEV-55] refactor: 글자 수, 본문 글자 수 세는 것까지  일단 저장
    
    * [DEV-55] feat: tiptap 추가
    
    * [DEV-55] feat: 노트 작성 부분 완성.. 마크 다운 방법 적용
    
    * [DEV-55] refactor: 코드 가독성 스타일 대거 수정
    
    * [DEV-55] remove: 예전 파일 삭제
    
    * [DEV-55] refactor: 코드래빗 피드백 반영
    
    * [DEV-57] feat: 링크 첨부 기능 구현
    
    * [DEV-57] refactor: 코드 리뷰 피드백 반영
    
    * [DEV-57] refactor: 2차 코드 피드백 반영

--- src/containers/note/toolbar.tsx
+++ src/containers/note/toolbar.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 import { Editor } from "@tiptap/react";
 
 import BoldIcon from "@/assets/editor/bold.svg";
@@ -9,12 +11,16 @@ import H3Icon from "@/assets/editor/h3.svg";
 import H4Icon from "@/assets/editor/h4.svg";
 import H5Icon from "@/assets/editor/h5.svg";
 import ItalicIcon from "@/assets/editor/italic.svg";
+import LinkIcon from "@/assets/editor/link.svg";
 import UnderlineIcon from "@/assets/editor/underline.svg";
+import Modal from "@/components/@common/portal/modal";
+import Button from "@/components/button";
 import { cn } from "@/lib/cn";
 import "@/styles/note.css";
 
 interface ToolbarProps {
   editor: Editor | null;
+  onLinkSubmit: (link: string) => void;
 }
 
 interface ToolbarButtonProps {
@@ -34,14 +40,34 @@ function ToolbarButton({
     <button
       onClick={onClick}
       className={cn("rounded p-2", { "bg-slate-400": isActive })}
+      style={{ width: "25px", height: "25px", marginRight: "4px" }}
     >
       <IconComponent className="h-25 w-25" aria-label={ariaLabel} />
     </button>
   );
 }
 
-function Toolbar({ editor }: ToolbarProps) {
+function Toolbar({ editor, onLinkSubmit }: ToolbarProps) {
+  const [isLinkModalOpen, setLinkModalOpen] = useState(false);
+  const [inputLink, setInputLink] = useState("");
+
   if (!editor) return null;
+
+  const openLinkModal = () => setLinkModalOpen(true);
+  const closeLinkModal = () => {
+    setLinkModalOpen(false);
+    const urlPattern =
+      /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\w .-]*)*\/?$/;
+    const trimmedInput = inputLink.trim();
+
+    if (trimmedInput && urlPattern.test(trimmedInput)) {
+      onLinkSubmit(trimmedInput);
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputLink(e.target.value);
+  };
 
   const buttonConfigs = [
     {
@@ -92,19 +118,71 @@ function Toolbar({ editor }: ToolbarProps) {
       ariaLabel: "H5",
       isActive: editor.isActive("heading", { level: 5 }),
     },
+    // 링크 버튼
+    {
+      onClick: openLinkModal,
+      IconComponent: LinkIcon,
+      ariaLabel: "Link",
+      isActive: false,
+    },
   ];
 
+  const linkInput = (
+    <input
+      type="text"
+      value={inputLink}
+      onChange={handleInputChange}
+      placeholder="https://"
+      className="h-44 w-full truncate rounded-12 bg-slate-50 px-24 py-12 text-sm outline-none md:h-48 md:text-base"
+    />
+  );
+
+  const confirmButton = (
+    <Button
+      variant="solid"
+      shape="square"
+      size="lg"
+      className="w-full"
+      onClick={closeLinkModal}
+    >
+      확인
+    </Button>
+  );
+
   return (
-    <div className="container-width fixed bottom-24 flex h-44 rounded-22 border-slate-200 bg-white p-2 pl-16 shadow">
-      {buttonConfigs.map((btn) => (
+    <div
+      className="container-width fixed bottom-24 flex h-44 items-center justify-center rounded-22 border-slate-200 bg-white p-2 pl-16 shadow"
+      style={{ justifyContent: "space-between" }}
+    >
+      <div style={{ display: "flex" }}>
+        {buttonConfigs.slice(0, -1).map((btn) => (
+          <ToolbarButton
+            key={btn.ariaLabel}
+            onClick={btn.onClick}
+            IconComponent={btn.IconComponent}
+            ariaLabel={btn.ariaLabel}
+            isActive={btn.isActive}
+          />
+        ))}
+      </div>
+      <div style={{ marginRight: "16px" }}>
         <ToolbarButton
-          key={btn.ariaLabel}
-          onClick={btn.onClick}
-          IconComponent={btn.IconComponent}
-          ariaLabel={btn.ariaLabel}
-          isActive={btn.isActive}
+          onClick={buttonConfigs[buttonConfigs.length - 1].onClick}
+          IconComponent={buttonConfigs[buttonConfigs.length - 1].IconComponent}
+          ariaLabel={buttonConfigs[buttonConfigs.length - 1].ariaLabel}
+          isActive={buttonConfigs[buttonConfigs.length - 1].isActive}
         />
-      ))}
+      </div>
+      {isLinkModalOpen && (
+        <Modal.Root open={isLinkModalOpen} onOpenChange={setLinkModalOpen}>
+          <Modal.Content className="w-311 md:w-520">
+            <Modal.Title className="text-bold text-lg">링크 업로드</Modal.Title>
+            <div className="mb-12 font-semibold">링크</div>
+            {linkInput}
+            <Modal.Footer>{confirmButton}</Modal.Footer>
+          </Modal.Content>
+        </Modal.Root>
+      )}
     </div>
   );
 }
