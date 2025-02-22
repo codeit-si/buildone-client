b147576faf0738a7cdf965e72a1767627531a4f9
typeYu <105202875+typeYu@users.noreply.github.com>
2025-02-22 21:47:11 +0900

    [Dev 86] feat: 목표 별 할일 노트 조회 (#126)
    * [DEV-86] refactor: 노트 모아보기 페이지 일단 저장
    
    * [DEV-86] refactor: css까지의 작업 중간 저장
    
    * [DEV-86] refactor: 경로 실수 수정
    
    * [DEV-86] refactor: 코드 컨벤션 준수

---
+++ src/app/notes/collection/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import FlagGoalSmall from "@/assets/flag_goal_small.svg";
+import Card from "@/components/note/card";
+import "@/styles/note.css";
+
+export default function NoteCollection() {
+  return (
+    <div className="container-width ml-80 mt-24">
+      <div className="text-lg font-semibold">노트 모아보기</div>
+      <div className="flex h-52 items-center rounded-12 bg-white pb-14 pl-24 pr-24 pt-14">
+        <FlagGoalSmall className="h-24 w-24" />
+        <span className="ml-8 text-sm font-semibold">
+          자바스크립트로 웹 서비스 만들기
+        </span>
+      </div>
+      <Card />
+    </div>
+  );
+}
--- src/components/note/todo.tsx
+++ src/components/note/todo.tsx
@@ -7,7 +7,7 @@ interface TodoProps {
 export default function Todo({ todoText }: TodoProps) {
   return (
     <div className="mb-24 flex items-center">
-      <div className="flex h-20 w-37 items-center justify-center rounded-4 bg-slate-100 text-xs text-slate-700">
+      <div className="flex h-20 w-37 items-center justify-center rounded-4 bg-slate-100 text-xs font-medium text-slate-700">
         To do
       </div>
       <div className="ml-8 text-sm">{todoText}</div>
