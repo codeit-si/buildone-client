7165d687e2d0a8fb267b5be706ffb9ef985335f2
hvrain <hvrain3222@naver.com>
2025-02-24 19:04:32 +0900

    [DEV-58] fix: 목표 별 할이 폰트 크기 수정

--- src/components/dashboard/todos-by-goal/goal-title.tsx
+++ src/components/dashboard/todos-by-goal/goal-title.tsx
@@ -7,5 +7,9 @@ export default function GoalTitle({
   children,
   className = "",
 }: PropsWithChildren<WithClassName>) {
-  return <h3 className={cn("text-lg font-bold", className)}>{children}</h3>;
+  return (
+    <h3 className={cn("text-base font-bold md:text-lg", className)}>
+      {children}
+    </h3>
+  );
 }
