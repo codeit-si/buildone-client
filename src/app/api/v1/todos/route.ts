5c3cf93c29a2f25863a24242198205611d252a96
hvrain <hvrain3222@naver.com>
2025-02-24 15:09:09 +0900

    [DEV-58] feat: 목표 리스트 mockDate 구현

---
+++ src/app/api/v1/todos/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest } from "next/server";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const mockTodoData = (goalId: any, cursor = 0, size = 10, done = false) => {
+  const totalCount = 100;
+
+  return {
+    paginationInformation: {
+      nextCursor: cursor + size < totalCount ? cursor + size : 0, // 예제에서는 최대 100개 데이터로 가정
+      totalCount, // 전체 데이터 개수 가정
+      hasNext: cursor + size < totalCount,
+    },
+    todos: Array.from({ length: size }, (_, index) => {
+      const id = cursor + index + 1;
+      return {
+        id,
+        noteId: 0,
+        title: `할 일 ${id}`,
+        goalInformation: {
+          id: goalId,
+          title: `목표 ${goalId}`,
+        },
+        linkUrl: "https://example.com",
+        fileUrl: "https://s3.apnortheast.com/",
+        isDone: done,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      };
+    }),
+  };
+};
+
+export const GET = async (request: NextRequest) => {
+  const { searchParams } = request.nextUrl;
+  const goalId = Number(searchParams.get("goalId"));
+  const cursor = Number(searchParams.get("cursor")) || 0;
+  const size = Number(searchParams.get("size")) || 5;
+  const done = searchParams.get("done") === "true";
+
+  const data = mockTodoData(goalId, cursor, size, done);
+
+  if (!data) {
+    return Response.json({
+      paginationInformation: {
+        nextCursor: 0,
+        totalCount: 0,
+        hasNext: false,
+      },
+      todos: [],
+    });
+  }
+  return Response.json(data);
+};
