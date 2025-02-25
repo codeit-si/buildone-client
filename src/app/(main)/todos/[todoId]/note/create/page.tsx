2ec13ee97f966f1bd5726dea6469c34ab4d6415a
typeyu <mytypeyu@gmail.com>
2025-02-25 14:24:10 +0900

    [DEV-80] refactor: 자잘한 수정

--- src/app/(main)/todos/[todoId]/note/create/page.tsx
+++ src/app/(main)/todos/[todoId]/note/create/page.tsx
@@ -62,86 +62,93 @@ export default function NotesPage() {
   };
 
   return (
-    <div className="ml-16 mt-24 md:ml-24 lg:ml-80">
-      <div className="container-width">
-        {/* 헤더 */}
-        <div className="grid h-44 grid-cols-[162px_auto] items-center">
-          <h1 className="truncate font-semibold text-slate-900 md:text-lg">
-            노트 작성
-          </h1>
-          <div className="flex justify-end gap-8">
-            <Button variant="outlined" size="sm" shape="square">
-              임시저장
-            </Button>
-            <Button variant="solid" size="sm" shape="square">
-              작성 완료
-            </Button>
-          </div>
-        </div>
-
-        <div className="mt-16">
-          {/* 임시 저장 노트 로드 토스트 */}
-          <LoadNoteToastManager onLoadNote={handleOpenLoadModal} />
-
-          {/* 목표 표시 */}
-          <Goal goalText="자바스크립트로 웹 서비스 만들기" />
-
-          {/* To do 항목 */}
-          <Todo todoText="자바스크립트 기초 챕터1 듣기" />
-
-          {/* 노트 제목 입력 */}
-          <div className="flex items-center gap-8 border-b border-t border-slate-200">
-            <input
-              type="text"
-              className="h-52 w-full text-lg placeholder:text-lg focus:outline-none"
-              placeholder="노트의 제목을 입력해주세요"
-              value={title}
-              onChange={handleTitleChange}
-            />
-            <Counting type="title" count={title.length} total={50} />
-          </div>
-
-          {/* 에디터 영역 */}
-          <div className="mt-12">
-            <Counting
-              type="text"
-              count={countWithoutSpaces(content)}
-              total={countWithSpaces(content)}
-            />
-
-            {/* 링크 첨부 */}
-            <div className="mt-28">
-              {showLink && (
-                <LinkAttached link={link} onRemove={handleRemoveLink} />
-              )}
+    <div className="min-h-screen bg-white">
+      <div className="ml-16 pt-24 md:ml-24 lg:ml-80">
+        <div className="container-width">
+          {/* 헤더 */}
+          <div className="grid h-44 grid-cols-[162px_auto] items-center">
+            <h1 className="truncate font-semibold text-slate-900 md:text-lg">
+              노트 작성
+            </h1>
+            <div className="flex justify-end gap-8">
+              <Button
+                variant="outlined"
+                size="sm"
+                shape="square"
+                className="border-0"
+              >
+                임시저장
+              </Button>
+              <Button variant="solid" size="sm" shape="square">
+                작성 완료
+              </Button>
             </div>
+          </div>
 
-            {/* 본문 */}
-            <div className="mt-16">
-              <Tiptap
-                content={content}
-                setContents={setContent}
-                onLinkSubmit={handleAddLink}
+          <div className="mt-16">
+            {/* 임시 저장 노트 로드 토스트 */}
+            <LoadNoteToastManager onLoadNote={handleOpenLoadModal} />
+
+            {/* 목표 표시 */}
+            <Goal goalText="자바스크립트로 웹 서비스 만들기" />
+
+            {/* To do 항목 */}
+            <Todo todoText="자바스크립트 기초 챕터1 듣기" />
+
+            {/* 노트 제목 입력 */}
+            <div className="flex items-center gap-8 border-b border-t border-slate-200">
+              <input
+                type="text"
+                className="h-52 w-full text-lg placeholder:text-lg focus:outline-none"
+                placeholder="노트의 제목을 입력해주세요"
+                value={title}
+                onChange={handleTitleChange}
               />
+              <Counting type="title" count={title.length} total={50} />
             </div>
-          </div>
-          <TempSaveManager />
-        </div>
-      </div>
 
-      {/* 모달 */}
-      {loadedNote && (
-        <LoadNoteModal
-          open={modalOpen}
-          setOpen={setModalOpen}
-          storedTitle={
-            loadedNote.title && loadedNote.title.trim() !== ""
-              ? loadedNote.title
-              : "제목 없음"
-          }
-          onLoad={() => handleLoadNote(loadedNote)}
-        />
-      )}
+            {/* 에디터 영역 */}
+            <div className="mt-12">
+              <Counting
+                type="text"
+                count={countWithoutSpaces(content)}
+                total={countWithSpaces(content)}
+              />
+
+              {/* 링크 첨부 */}
+              <div className="mt-28">
+                {showLink && (
+                  <LinkAttached link={link} onRemove={handleRemoveLink} />
+                )}
+              </div>
+
+              {/* 본문 */}
+              <div className="mt-16">
+                <Tiptap
+                  content={content}
+                  setContents={setContent}
+                  onLinkSubmit={handleAddLink}
+                />
+              </div>
+            </div>
+            <TempSaveManager />
+          </div>
+        </div>
+
+        {/* 모달 */}
+        {loadedNote && (
+          <LoadNoteModal
+            open={modalOpen}
+            setOpen={setModalOpen}
+            storedTitle={
+              loadedNote.title && loadedNote.title.trim() !== ""
+                ? loadedNote.title
+                : "제목 없음"
+            }
+            onLoad={() => handleLoadNote(loadedNote)}
+          />
+        )}
+      </div>
     </div>
   );
 }
