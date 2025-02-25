d850410d14c2d6279f2a308483048edd8985e5b5
hvrain <hvrain3222@naver.com>
2025-02-25 17:23:55 +0900

    [DEV-0001] feat: 코드래빗 config 테스트

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -3,6 +3,8 @@ import RecentlyTodoContainer from "@/components/dashboard/recently-todo/recently
 import TodosByGoalContainer from "@/components/dashboard/todos-by-goal/todos-by-goal-container";
 
 export default function DashboardPage() {
+  // eslint-disable-next-line no-console
+  console.log("dashboard");
   return (
     <div className="grid h-full min-h-screen w-full grid-rows-[repeat(2,_258px)_1fr] gap-x-24 gap-y-8 overflow-y-scroll bg-slate-100 px-16 md:grid-cols-2 md:grid-rows-[auto_250px_1fr]">
       <h1 className="hidden text-base font-semibold md:col-span-2 md:block md:text-lg">
