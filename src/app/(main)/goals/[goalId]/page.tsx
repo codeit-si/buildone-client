dd113095f955407413211d0faa3431a7efbc3871
ChaeYubin <cyoobin@naver.com>
2025-02-26 19:06:55 +0900

    [DEV-39] feat: 목표 상세 페이지 무한스크롤 조회 로직 추가

--- src/app/(main)/goals/[goalId]/page.tsx
+++ src/app/(main)/goals/[goalId]/page.tsx
@@ -1,11 +1,12 @@
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-import CompletedTodoList from "@/components/goal-detail/completed-todo-list";
 import GoalSummary from "@/components/goal-detail/goal-summary";
 import RouteButtonToNotes from "@/components/goal-detail/route-button-to-notes";
-import UncompletedTodoList from "@/components/goal-detail/uncompleted-todo-list";
+import TodoList from "@/components/goal-detail/todo-list";
 import getQueryClient from "@/lib/get-query-client";
+import { getProgressByGoalIdOptions } from "@/services/dashboard/query";
 import { getGoalOptions } from "@/services/goal/query";
+import { getInfiniteTodosByGoalIdOptions } from "@/services/todo/query";
 
 export default async function GoalDetailPage({
   params,
@@ -13,19 +14,39 @@ export default async function GoalDetailPage({
   params: { goalId: string };
 }) {
   const queryClient = getQueryClient();
+
   await queryClient.prefetchQuery(getGoalOptions(Number(params.goalId)));
+
+  await queryClient.prefetchQuery(
+    getProgressByGoalIdOptions(Number(params.goalId)),
+  );
+
+  await queryClient.prefetchInfiniteQuery(
+    getInfiniteTodosByGoalIdOptions({
+      goalId: Number(params.goalId),
+      done: true,
+    }),
+  );
+
+  await queryClient.prefetchInfiniteQuery(
+    getInfiniteTodosByGoalIdOptions({
+      goalId: Number(params.goalId),
+      done: false,
+    }),
+  );
+
   const dehydratedState = dehydrate(queryClient);
 
   return (
     <HydrationBoundary state={dehydratedState}>
       <div className="w-343 py-16 max-lg:mx-auto md:w-636 md:py-24 lg:ml-80 lg:w-1200">
         <h1 className="hidden text-lg font-semibold md:block">목표</h1>
-        <div className="flex flex-col gap-y-16 md:gap-y-24">
+        <div className="flex flex-col gap-16 md:gap-24">
           <GoalSummary goalId={params.goalId} />
           <RouteButtonToNotes goalId={params.goalId} />
-          <div className="flex flex-col gap-y-24 lg:flex-row lg:gap-x-24">
-            <UncompletedTodoList goalId={params.goalId} />
-            <CompletedTodoList goalId={params.goalId} />
+          <div className="flex flex-col gap-16 md:gap-24 lg:flex-row lg:items-start">
+            <TodoList goalId={params.goalId} done={false} />
+            <TodoList goalId={params.goalId} done />
           </div>
         </div>
       </div>
--- src/components/goal-detail/goal-summary.tsx
+++ src/components/goal-detail/goal-summary.tsx
@@ -3,6 +3,7 @@
 import { useQuery } from "@tanstack/react-query";
 
 import FlagGoalIcon from "@/assets/icons-big/flag_goal.svg";
+import { getProgressByGoalIdOptions } from "@/services/dashboard/query";
 import { getGoalOptions } from "@/services/goal/query";
 
 import ProgressBar from "../@common/progress-bar";
@@ -14,20 +15,29 @@ interface GoalSummaryProps {
 }
 
 export default function GoalSummary({ goalId }: GoalSummaryProps) {
-  const { data } = useQuery(getGoalOptions(Number(goalId)));
+  const { data: goalData } = useQuery(getGoalOptions(Number(goalId)));
+  const { data: progressData } = useQuery(
+    getProgressByGoalIdOptions(Number(goalId)),
+  );
 
   return (
     <div className="mt-0 rounded-12 border border-slate-100 bg-white px-24 py-16 md:mt-16">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-x-8">
           <FlagGoalIcon />
-          <h2 className="text-base font-semibold md:text-lg">{data?.title}</h2>
+          <h2 className="text-base font-semibold md:text-lg">
+            {goalData?.title}
+          </h2>
         </div>
         <GoalSummaryDropdown goalId={goalId} />
       </div>
       <div className="mt-24">
         <p className="mb-8 text-xs font-semibold">Progress</p>
-        <ProgressBar current={20} total={30} border={false} />
+        <ProgressBar
+          current={progressData?.progress ?? 0}
+          total={100}
+          border={false}
+        />
       </div>
     </div>
   );
