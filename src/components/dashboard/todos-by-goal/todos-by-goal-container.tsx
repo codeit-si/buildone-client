86b8833f6a1931006c018760998a5ac8d31b51bb
hvrain <hvrain3222@naver.com>
2025-02-27 18:00:34 +0900

    [DEV-94] fix: 목표 별 할일 목표 리스트 요청 부분 이동

--- src/components/dashboard/todos-by-goal/todos-by-goal-container.tsx
+++ src/components/dashboard/todos-by-goal/todos-by-goal-container.tsx
@@ -1,32 +1,31 @@
 "use client";
 
-import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 import FlagIcon from "@/assets/flag.svg";
-import { useInfiniteScroll } from "@/hooks/use-infinite-scroll";
-import { getInfiniteGoalsOptions } from "@/services/dashboard/query";
 
+import DashboardLoading from "../dashboard-loading";
 import SectionContainer from "../section-container";
 import SectionTitle from "../section-title";
 
 import GoalList from "./goal-list";
 
 export default function TodosByGoalContainer() {
-  const { data, fetchNextPage, hasNextPage } = useSuspenseInfiniteQuery(
-    getInfiniteGoalsOptions({}),
-  );
-  const { ref } = useInfiniteScroll({ hasNextPage, fetchNextPage });
-
   return (
-    <SectionContainer className="mt-16 md:col-span-2">
+    <SectionContainer className="mt-16 min-h-200 md:col-span-2 md:min-h-511">
       <SectionTitle>
         <div className="flex h-40 w-40 items-center justify-center rounded-15 bg-blue-500">
           <FlagIcon />
         </div>
         <h2 className="line-clamp-1">목표 별 할일</h2>
       </SectionTitle>
-      {data?.pages && <GoalList goals={data.pages} />}
-      {hasNextPage && <div ref={ref}>목표 로딩 중...</div>}
+      <Suspense
+        fallback={
+          <DashboardLoading>목표를 불러오는 중입니다...</DashboardLoading>
+        }
+      >
+        <GoalList />
+      </Suspense>
     </SectionContainer>
   );
 }
