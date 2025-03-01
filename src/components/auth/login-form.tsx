d4db3e31be1eade01128ca51ad31eed8433fb88a
xorms571 <83347911+xorms571@users.noreply.github.com>
2025-03-01 16:49:27 +0900

    [DEV-107] feat: 계정 정보 출력 (#182)

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -67,7 +67,7 @@ export default function LoginForm() {
       setAccessToken(token);
       setUserInfo(response.data.memberInformation);
 
-      router.push("/");
+      router.push("/dashboard");
     } catch (error: unknown) {
       if (error instanceof ApiError) {
         if (
