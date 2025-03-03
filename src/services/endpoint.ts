84875a8297bed031f0b0c7da2e02dab9adec88ee
ChaeYubin <cyoobin@naver.com>
2025-03-03 15:44:09 +0900

    [DEV-126] fix: 할일 진행률 조회 API 경로 수정

--- src/services/endpoint.ts
+++ src/services/endpoint.ts
@@ -12,7 +12,7 @@ export const ENDPOINT = {
     CREATE: `${PREFIX}/goals`,
     UPDATE: (id: number) => `${PREFIX}/goals/${id}`,
     DELETE: (id: number) => `${PREFIX}/goals/${id}`,
-    GET_PROGRESS: (id: number) => `${PREFIX}/goals/progress/${id}`,
+    GET_PROGRESS: `${PREFIX}/goals/progress`,
   },
   TODO: {
     GET_ALL: `${PREFIX}/todos`,
