0cfdf87e760403cbff7bb6150dd9a0efc6c06bc4
hvrain <hvrain3222@naver.com>
2025-03-06 14:02:26 +0900

    [DEV-95] feat: api 연결하여 도넛 그래프에 적용

--- src/components/dashboard/my-progress/my-progress-container.tsx
+++ src/components/dashboard/my-progress/my-progress-container.tsx
@@ -1,5 +1,7 @@
 // import StreakBoard from "@/components/dashboard/streak-board/streak-board";
 
+import EllipseIcon from "@/assets/icons-big/ellipse.svg";
+
 import SectionContainer from "../section-container";
 
 import MyProgress from "./my-progress";
@@ -7,7 +9,12 @@ import MyProgress from "./my-progress";
 export default async function MyProgressContainer() {
   return (
     <SectionContainer className="bg-dark-blue-500">
-      <MyProgress />
+      <div className="relative h-full w-full">
+        <MyProgress />
+        <div className="absolute -bottom-16 -right-16">
+          <EllipseIcon />
+        </div>
+      </div>
       {/* <StreakBoard /> */}
     </SectionContainer>
   );
