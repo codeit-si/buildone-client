4d7fa1f972bfa24df46c9a0710ecbdb65933ecb6
ChaeYubin <cyoobin@naver.com>
2025-03-13 15:26:37 +0900

    [DEV-116] refactor: label을 사용하는 input을 labeledInput으로 수정

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -8,12 +8,13 @@ import { useRouter } from "next/navigation";
 import { z } from "zod";
 
 import Button from "@/components/@common/button";
-import Input from "@/components/@common/input";
 import { LOGIN_ERROR_CODE } from "@/constants/error";
 import { useDebounce } from "@/hooks/use-debounce";
 import { ApiError } from "@/lib/error";
 import { login } from "@/services/auth";
 import { useUserStore } from "@/store/user-store";
+
+import LabeledInput from "../@common/input/labeled-input";
 
 const loginSchema = z.object({
   email: z
@@ -93,7 +94,7 @@ export default function LoginForm() {
         },
       ].map(({ key, label, placeholder }, index) => (
         <div className={index > 0 ? "mt-24" : ""} key={key}>
-          <Input
+          <LabeledInput
             id={key}
             label={label}
             type={key.includes("password") ? "password" : "text"}
@@ -104,6 +105,17 @@ export default function LoginForm() {
               "border-red-500 focus-within:border-red-500 hover:border-red-500"
             }
           />
+          {/* <Input
+            id={key}
+            label={label}
+            type={key.includes("password") ? "password" : "text"}
+            placeholder={placeholder}
+            {...register(key as LoginSchemaKey)}
+            className={
+              errors[key as LoginSchemaKey] &&
+              "border-red-500 focus-within:border-red-500 hover:border-red-500"
+            }
+          /> */}
           {errors[key as LoginSchemaKey] && (
             <p className="mt-8 inline-block text-sm font-normal text-red-500">
               {errors[key as LoginSchemaKey]?.message}
