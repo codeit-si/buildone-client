b189d896c83aebbf50c4d6d57b93d559dc9f09d5
ChaeYubin <cyoobin@naver.com>
2025-03-14 15:13:09 +0900

    [DEV-42] feat: fcm 토큰 store 저장 로직 추가

--- src/lib/fcm-provider.tsx
+++ src/lib/fcm-provider.tsx
@@ -7,40 +7,44 @@ import { useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken } from "firebase/messaging";
 
+import { useUserStore } from "@/store/user-store";
+
 export default function FcmProvider() {
-  const onMessageFCM = async () => {
-    const permission = await Notification.requestPermission();
-
-    if (permission !== "granted") return;
-
-    const firebaseConfig = {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-    };
-
-    const firebaseApp = initializeApp(firebaseConfig);
-
-    const messaging = getMessaging(firebaseApp);
-
-    getToken(messaging, {
-      vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
-    })
-      .then((token: string) => {
-        console.log(`푸시 토큰 발급 완료: ${token}`);
-      })
-      .catch((err: unknown) => {
-        console.log("An error occurred while retrieving token. ", err);
-      });
-  };
+  const { setFcmToken } = useUserStore();
 
   useEffect(() => {
+    const onMessageFCM = async () => {
+      const permission = await Notification.requestPermission();
+
+      if (permission !== "granted") return;
+
+      const firebaseConfig = {
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+      };
+
+      const firebaseApp = initializeApp(firebaseConfig);
+
+      const messaging = getMessaging(firebaseApp);
+
+      getToken(messaging, {
+        vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
+      })
+        .then((token: string) => {
+          setFcmToken(token);
+        })
+        .catch((err: unknown) => {
+          console.log("An error occurred while retrieving token. ", err);
+        });
+    };
+
     onMessageFCM();
-  }, []);
+  }, [setFcmToken]);
 
   // eslint-disable-next-line react/jsx-no-useless-fragment
   return <></>;
