f84188391bf2f1e356f1995311f59e37bcc96445
yubin <cyoobin@naver.com>
2025-03-16 21:36:24 +0900

    [Dev 42] 푸시 알림 기능 관련 API & 버그 수정 (#298)
    * [DEV-42] fix: import 구문 삭제, 환경변수 하드코딩 값으로 수정
    
    * [DEV-42] fix: fcm 토큰 등록 API memberId 파라미터 추가 및 디렉토리, 변수 이름 구체적으로 수정
    
    * [DEV-42] fix: 빌드 에러 수정

--- src/services/push-notification/index.ts
+++ src/services/push-notification/index.ts
@@ -3,14 +3,14 @@ import { api } from "@/lib/axios";
 import { ENDPOINT } from "../endpoint";
 
 /** FCM 웹 푸시 토큰 등록 API */
-export const registerToken = async (token: string) => {
-  const res = await api.post(ENDPOINT.PUSH.REGISTER_TOKEN, { token });
+export const registerFcmToken = async (token: string, memberId: number) => {
+  const res = await api.post(ENDPOINT.PUSH.REGISTER_TOKEN, { token, memberId });
 
   return res;
 };
 
 /** FCM 웹 푸시 알림 발송 API */
-export const sendPush = async (
+export const sendPushNotification = async (
   memberId: number,
   title: string,
   body: string,
