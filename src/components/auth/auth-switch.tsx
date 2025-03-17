6801fac32fab0eb47ffd85d007eabe3105b5fc8c
yubin <cyoobin@naver.com>
2025-03-17 14:35:47 +0900

    [Dev 169] 로그인 이후 로딩 표시 추가 (#308)
    * [DEV-169] refactor: auth-switch 컴포넌트 props 이름 수정
    
    * [DEV-169] feat: 로그인 로딩 스피너 추가
    
    ---------
    
    Co-authored-by: typeyu <mytypeyu@gmail.com>

--- src/components/auth/auth-switch.tsx
+++ src/components/auth/auth-switch.tsx
@@ -1,18 +1,18 @@
 import Link from "next/link";
 
 interface AuthSwitchProps {
-  isLoginPage: boolean;
+  loginPage?: boolean;
 }
 
-export default function AuthSwitch({ isLoginPage }: AuthSwitchProps) {
+export default function AuthSwitch({ loginPage = false }: AuthSwitchProps) {
   return (
     <p className="flex justify-center gap-x-4 text-sm font-medium">
-      {isLoginPage ? "빌던이 처음이신가요?" : "이미 회원이신가요?"}
+      {loginPage ? "빌던이 처음이신가요?" : "이미 회원이신가요?"}
       <Link
-        href={isLoginPage ? "/signup" : "/login"}
+        href={loginPage ? "/signup" : "/login"}
         className="text-dark-blue-500 underline"
       >
-        {isLoginPage ? "회원가입" : "로그인"}
+        {loginPage ? "회원가입" : "로그인"}
       </Link>
     </p>
   );
