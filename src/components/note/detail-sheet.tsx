e9c638d833c62eb5bb25106070bb7a450d214778
hvrain <hvrain3222@naver.com>
2025-03-17 13:44:49 +0900

    [DEV-171] feat: 노트 시트 수정하기, 삭제하기 기능 추가

--- src/components/note/detail-sheet.tsx
+++ src/components/note/detail-sheet.tsx
@@ -5,17 +5,20 @@ import ReactMarkdown from "react-markdown";
 
 import { useQuery } from "@tanstack/react-query";
 import "github-markdown-css";
+import { useRouter } from "next/navigation";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
 
 import Sheet from "@/components/@common/portal/sheet";
 import Goal from "@/components/note/goal";
 import Todo from "@/components/note/todo";
+import { useDeleteNote } from "@/hooks/query/use-notes";
 import { getNote } from "@/services/note";
 import { noteKeys } from "@/services/query-key";
 import { NoteResponse } from "@/types/note";
 
 import EmbeddedFrame from "../@common/embeded-frame";
+import Popup from "../@common/portal/popup";
 
 import LinkAttached from "./link-attached";
 
@@ -31,13 +34,19 @@ export default function DetailSheet({
   noteId,
   linkUrl,
 }: DetailSheetProps): JSX.Element {
+  const router = useRouter();
   const { data: note, error } = useQuery<NoteResponse, Error>({
     queryKey: noteKeys.detail(noteId),
     queryFn: () => getNote(noteId),
   });
   const [sheetOpen, setSheetOpen] = useState(false);
+  const { mutate: deleteNote } = useDeleteNote();
 
   if (error || !note) return <div>Error loading detail.</div>;
+
+  const handleRoute = () => {
+    router.push(`/todos/9/note/write?noteId=${noteId}`);
+  };
 
   return (
     <Sheet.Content className="gap-0 p-24">
@@ -64,8 +73,33 @@ export default function DetailSheet({
         </ReactMarkdown>
       </div>
       <div className="mt-16 flex gap-8 self-end">
-        <button className={SHEET_BUTTON_CLASS}>수정하기</button>
-        <button className={SHEET_BUTTON_CLASS}>삭제하기</button>
+        <button onClick={handleRoute} className={SHEET_BUTTON_CLASS}>
+          수정하기
+        </button>
+        <Popup.Root>
+          <Popup.Trigger asChild>
+            <button className={SHEET_BUTTON_CLASS}>삭제하기</button>
+          </Popup.Trigger>
+          <Popup.Content>
+            <div className="text-center text-base font-medium text-slate-800">
+              노트를 삭제하시겠어요?
+              <br />
+              삭제된 노트는 복구할 수 없습니다.
+            </div>
+            <Popup.Footer>
+              <Popup.Close variant="outlined" className="w-120 min-w-120">
+                취소
+              </Popup.Close>
+              <Popup.Close
+                variant="solid"
+                className="w-120 min-w-120"
+                onClick={(): void => deleteNote(noteId)}
+              >
+                삭제
+              </Popup.Close>
+            </Popup.Footer>
+          </Popup.Content>
+        </Popup.Root>
       </div>
     </Sheet.Content>
   );
